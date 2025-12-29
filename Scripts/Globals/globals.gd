f26854407648f1dd44a690262f816613ddf3a4b0
TakaVII <ulirub@gmail.com>
2025-12-29 19:19:45 -0400

    Add initial game assets and config files
    Added .editorconfig, .gitattributes, and updated .gitignore. Introduced a large set of image and import files for bosses, heroes, enemies, and fonts, as well as their corresponding import metadata. These assets are required for the game's visual and font resources.

---
+++ Scripts/Globals/globals.gd
@@ -0,0 +1,22 @@
+extends Node
+
+enum States {
+	FIELD,
+	BATTLE
+}
+
+const GAME_SIZE: Vector2 = Vector2( 320, 160 )
+const CELL_SIZE: Vector2 = Vector2( 16, 16 )
+
+var current_map: TileMapLayer = null
+var target_cell: Vector2 = Vector2.ZERO
+# var text_box: Textbox = null
+var camera: Camera2D = null
+# var screen_shake: ScreenShake2D = null
+var music_position: float = 0.0
+var state: int = 0
+
+func _ready() -> void:
+	randomize()
+	process_mode = PROCESS_MODE_ALWAYS
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
