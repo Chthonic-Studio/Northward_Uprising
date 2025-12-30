f23a3b8a6a0412f33db88d28bc1af85e924bc374
TakaVII <ulirub@gmail.com>
2025-12-30 13:31:30 -0400

    Remove duplicate character, enemy, boss, and tileset assets
    Deleted all image and import files for characters, enemies, bosses, and tilesets from the Rogue Adventure World project.

--- Resources/unit_resource.gd
+++ Resources/unit_resource.gd
@@ -1,11 +1,10 @@
-extends Node
+class_name UnitResource extends Resource
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@export_category("Basic Info & Stats")
+@export var character_name: String = "Unit"
+@export var movement_range: int = 5
+@export var level: int = 1
+@export var experience: int = 0
+@export var max_health: int = 10
+@export var attack: int = 5
+@export var defense: int = 2
