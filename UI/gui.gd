9e4f0279be808319869d9a77a67656c8019705cb
TakaVII <ulirub@gmail.com>
2025-12-31 18:02:41 -0400

    Refactor actor info panels in GUI
    Renamed ActorInfo to HighlightedActor and added a new ActiveActor panel in the scene. Updated gui.gd to reference the new label nodes for both highlighted and active actors, improving clarity and separation of actor states in the UI.

--- UI/gui.gd
+++ UI/gui.gd
@@ -1,7 +1,9 @@
 extends Node
 
-@onready var actor_name: Label = $Control/ActorInfo/HBoxContainer/VBoxContainer/ActorName
-@onready var actor_hp: Label = $Control/ActorInfo/HBoxContainer/VBoxContainer/ActorHP
+@onready var h_actor_name: Label = $Control/HighlightedActor/HBoxContainer/VBoxContainer/H_ActorName
+@onready var h_actor_hp: Label = $Control/HighlightedActor/HBoxContainer/VBoxContainer/H_ActorHP
+@onready var a_actor_name: Label = $Control/ActiveActor/HBoxContainer/VBoxContainer/A_ActorName
+@onready var a_actor_hp: Label = $Control/ActiveActor/HBoxContainer/VBoxContainer/A_ActorHP
 
 
 # Called when the node enters the scene tree for the first time.
